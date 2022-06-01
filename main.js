88d70f0f0b8a74ae4268fd90f659befce8c600fc
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-01 15:41:47 +0300

    1

---
+++ main.js
@@ -0,0 +1,12 @@
+// function getAlert (){
+//     alert("hi mami")
+// }
+// function printName (){
+//     console.log("daniel gadamo");
+// }
+// function getColor(){
+//     spani.style.color = "yellow" ;
+// }
+function getText (){
+    text1.style.fontsize = "30px"
+}
